44da8065580fab0e4e8f4c783d68671fb7e29f69
github username <github email address>
2019-03-11 10:59:21 -0500

    building seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
-jeff = User.create(name: "jfk248", )
-Pet.create(name: "Maddy", owner: sophie)
-Pet.create(name: "Nona", owner: sophie)
+User.create(username: "jfk248", email: "jfk248@gmail.com", password: "jfk123")
+Destination.create(name: "Honolulu", description: "", star_ranking: '10', image: "")
+Destination.create(name: "Belize", description: "",star_ranking: '8', image: "")
+Destination.create(name: "Punta Cana", description: "",star_ranking: '9', image: "")
+Destination.create(name: "Ibiza", description: "",star_ranking: '10', image: "")
+Destination.create(name: "Puerto Rico", description: "",star_ranking: '7', image: "")
+
+User.create(username: "trvllover", email: "trvlover@gmail.com", password: "abc123")
+Destination.create(name: "Vienna", description: "", star_ranking: '10', image: "")
+Destination.create(name: "Barcelona", description: "",star_ranking: '9', image: "")
+Destination.create(name: "Paris", description: "",star_ranking: '9', image: "")
+Destination.create(name: "London", description: "",star_ranking: '10', image: "")
+Destination.create(name: "Munich", description: "",star_ranking: '8', image: "")
+
